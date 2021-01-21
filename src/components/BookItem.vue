3918eae00661cd3b0f8967ad8b7de265e4576de4
DUANGRUTHAI\Duangruthai <614234005@parichat.skru.ac.th>
2021-01-21 10:19:55 +0700

    แสดงรายละเอียดหนังสือแต่ละเล่ม โดยใช้ Data

---
+++ src/components/BookItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div class="book-item">
+      <h1>{{ book.title }}</h1>
+
+      <div style="width: 100%; text-align: left">
+        <img
+          style="float: left; padding: 20px"
+          width="150px"
+          v-bind:src="book.thumbnailUrl"
+        />
+        <div style="padding:20px;">
+          <b>Author:</b>{{ book.author }}<br />
+          <b>Category:</b>{{ book.category }}<br />
+          <b>ISBN:</b>{{ book.isbn }}<br />
+          <b>Page:</b>{{ book.pageCount }}<br />
+          <b>Description:</b><br />
+          <p>{{ book.shortDescription }}</p>
+        </div>
+      </div>
+      <br />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BookItem",
+  components: {},
+  props: ["book"],
+};
+</script>
+
+
+
+<style>
+.book-item {
+  background: #f4f4f4;
+  padding: 50px;
+  border-bottom: 1px #cccccc dotted;
+}
+</style>
